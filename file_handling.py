c71d8cfc9a5dab4116cb3efa3baf357d1bd5074f
Voste254 <onyangosteveok@gmail.com>
2026-07-23 16:31:53 +0300

    feat: add entry to data.txt and update file_handling.py to use read-write mode with commented-out trial processing

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,7 @@
-file1 = open('data.txt','r')
+file1 = open('data.txt','r+')
+# t = open('trial.txt','r+')
 data= file1.readlines()
-print(data[2])
+# print(data[2])
+
+# for d in data:
+#     t.write(d)
